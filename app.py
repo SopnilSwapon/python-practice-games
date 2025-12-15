1bbca00d87a30ad336f869b8018938a38431aed3
SopnilSwapon <sopnilswapon1@gmail.com>
2025-12-15 14:53:39 +0600

    created roll dice game

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
+import random
+
+
 def game():
-    print("Game building starts")
+    guessedCharacter = input("Roll the dice? (y/n): ")
+    if guessedCharacter.lower() == "n":
+        print("Thanks for playing")
+
+    elif guessedCharacter == "y":
+        print((random.randrange(1, 7), random.randrange(1, 7)))
+        game()
+
+    elif guessedCharacter != 'n' and guessedCharacter != 'y':
+        print("Invalid choice!")
+        game()
 
 
 game()
