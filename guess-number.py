130eb68ea91d87a55be3eefce0f3608d76bf3d2e
SopnilSwapon <sopnilswapon1@gmail.com>
2025-12-16 17:02:01 +0600

    added the error boundary for error handling properly

--- guess-number.py
+++ guess-number.py
@@ -3,13 +3,14 @@ import random
 guess_to_number = random.randint(1, 100)
 
 while True:
-    user_input_number = input("Your guess number?: ")
-    if int(user_input_number) > guess_to_number:
-        print("It's too High!")
-    elif int(user_input_number) < guess_to_number:
-        print("It's too low!")
-    elif int(user_input_number) == guess_to_number:
-        print("Congratulations, You guess it!!")
-        break
-    else:
-        print("Provided wrong input")
+    try:
+       user_input_number = input("Your guess number?: ")
+       if int(user_input_number) > guess_to_number:
+          print("It's too High!")
+       elif int(user_input_number) < guess_to_number:
+          print("It's too low!")
+       else:
+          print("Congratulations, You guess it!!")
+          break
+    except ValueError:
+        print("Please enter a valid number")
